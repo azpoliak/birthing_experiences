13391b3a8b61dd57facea34306762a995364471c
Noreen Mayat <nm3224@barnard.edu>
2021-07-01 14:17:18 -0400

    Added import tools

--- src/notebooks/imports.py
+++ src/notebooks/imports.py
@@ -4,6 +4,8 @@ import os
 import nltk
 from nltk import ngrams
 from nltk import tokenize
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 import numpy as np
 from datetime import datetime
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
@@ -14,7 +16,7 @@ import seaborn
 import redditcleaner
 import re
 import warnings
+import itertools
 warnings.filterwarnings("ignore")
-nltk.download('stopwords')
 
 birth_stories_df = pd.read_pickle('all_birth_stories.pkl')
