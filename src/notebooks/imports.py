2e10c046dc4254fbff7ee8fb2fb68f388454c245
Daphna Spira <des2188@barnard.edu>
2021-07-01 14:23:00 -0400

    added import itertools

--- src/notebooks/imports.py
+++ src/notebooks/imports.py
@@ -1,13 +1,16 @@
 import pandas as pd
-import little_mallet_wrapper
+import little_mallet_wrapper as lmw
 import os
 import nltk
 from nltk import ngrams
 from nltk import tokenize
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 import numpy as np
 from datetime import datetime
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from matplotlib import pyplot as plt
+import itertools
 from itertools import chain, zip_longest
 from little_mallet_wrapper import process_string
 import seaborn
@@ -15,6 +18,5 @@ import redditcleaner
 import re
 import warnings
 warnings.filterwarnings("ignore")
-nltk.download('stopwords')
 
 birth_stories_df = pd.read_pickle('all_birth_stories.pkl')
