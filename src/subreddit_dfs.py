af7a4de06ad585b636754d48309910796003afa6
Daphna Spira <des2188@barnard.edu>
2021-07-08 15:08:26 -0400

    connects comments to empty posts

--- src/subreddit_dfs.py
+++ src/subreddit_dfs.py
@@ -3,6 +3,21 @@ import os
 import numpy as np
 import nltk
 import compress_json
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+import seaborn as sns
+# Use seaborn style defaults and set the default figure size
+sns.set(rc={'figure.figsize':(11, 4)})
+import warnings
+warnings.filterwarnings("ignore")
+
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+
+from collections import defaultdict
+from tqdm import tqdm
 #import Maria_paper
 #from Maria_paper import birthstories
 
@@ -11,6 +26,8 @@ def birthstories(series):
     if 'birth story' in lowered:
         return True
     if 'birth stories' in lowered:
+        return True
+    if 'graduat' in lowered:
         return True
     else:
         return False
@@ -21,6 +38,32 @@ def story_lengths(series):
     tokenized = nltk.word_tokenize(lowered)
     length = len(tokenized)
     return length
+
+#translate created_utc column into years
+def get_post_year(series):
+    parsed_date = datetime.utcfromtimestamp(series)
+    date = parsed_date
+    return date
+
+#True/False column based on before and after pandemic 
+def pandemic(date):
+    start_date = datetime.strptime("11 March, 2020", "%d %B, %Y")
+    if date > start_date:
+        return False
+    else:
+        return True
+
+def get_first_comment(row):
+    curr_id, author = row.id, row.author
+    if not os.path.exists(f"..data/original-reddit/subreddits/BabyBumps/comments/{curr_id}.json.gz"):
+        return 
+    comments_df = pd.read_json(f"../data/original-reddit/subreddits/BabyBumps/comments/{curr_id}.json.gz", compression='gzip')
+    if comments_df.shape[0] == 0:
+        return
+    match_df = comments_df[(comments_df['parent_id'].map(lambda x: curr_id in x)) & (comments_df['author'] == author)].sort_values('created_utc',ascending=True)
+    if match_df.shape[0] == 0:
+        return 
+    return match_df.iloc[0]['body']
 
 #return if a story is 500+ words long or not
 def long_stories(series):
@@ -47,7 +90,7 @@ def main():
 
     BabyBumps_df.reset_index(drop=True, inplace=True)
     BabyBumps_df_j = BabyBumps_df.to_json()
-    compress_json.dump(BabyBumps_df_j, "BabyBumps_df.json.gz")
+    compress_json.dump(BabyBumps_df_j, "subreddit_json_gzs/BabyBumps_df.json.gz")
 
     beyond_the_bump_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/beyondthebump/submissions/"):
@@ -61,7 +104,7 @@ def main():
 
     beyond_the_bump_df.reset_index(drop=True, inplace=True)
     beyond_the_bump_df_j = beyond_the_bump_df.to_json()
-    compress_json.dump(beyond_the_bump_df_j, "beyond_the_bump_df.json.gz")
+    compress_json.dump(beyond_the_bump_df_j, "subreddit_json_gzs/beyond_the_bump_df.json.gz")
 
     BirthStories_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/BirthStories/submissions/"):
@@ -75,7 +118,7 @@ def main():
 
     BirthStories_df.reset_index(drop=True, inplace=True)
     BirthStories_df_j = BirthStories_df.to_json()
-    compress_json.dump(BirthStories_df_j, "BirthStories_df.json.gz")
+    compress_json.dump(BirthStories_df_j, "subreddit_json_gzs/BirthStories_df.json.gz")
 
     daddit_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/daddit/submissions/"):
@@ -89,7 +132,7 @@ def main():
 
     daddit_df.reset_index(drop=True, inplace=True)
     daddit_df_j = daddit_df.to_json()
-    compress_json.dump(daddit_df_j, "daddit_df.json.gz")
+    compress_json.dump(daddit_df_j, "subreddit_json_gzs/daddit_df.json.gz")
 
     predaddit_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/predaddit/submissions/"):
@@ -103,7 +146,7 @@ def main():
 
     predaddit_df.reset_index(drop=True, inplace=True)
     predaddit_df_j = predaddit_df.to_json()
-    compress_json.dump(predaddit_df_j, "predaddit_df.json.gz")
+    compress_json.dump(predaddit_df_j, "subreddit_json_gzs/predaddit_df.json.gz")
 
     pregnant_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/pregnant/submissions/"):
@@ -117,7 +160,7 @@ def main():
 
     pregnant_df.reset_index(drop=True, inplace=True)
     pregnant_df_j = pregnant_df.to_json()
-    compress_json.dump(pregnant_df_j, "pregnant_df.json.gz")
+    compress_json.dump(pregnant_df_j, "subreddit_json_gzs/pregnant_df.json.gz")
 
     Mommit_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/Mommit/submissions/"):
@@ -131,7 +174,7 @@ def main():
 
     Mommit_df.reset_index(drop=True, inplace=True)
     Mommit_df_j = Mommit_df.to_json()
-    compress_json.dump(Mommit_df_j, "Mommit_df.json.gz")
+    compress_json.dump(Mommit_df_j, "subreddit_json_gzs/Mommit_df.json.gz")
 
     NewParents_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/NewParents/submissions/"):
@@ -145,7 +188,7 @@ def main():
 
     NewParents_df.reset_index(drop=True, inplace=True)
     NewParents_df_j = NewParents_df.to_json()
-    compress_json.dump(NewParents_df_j, "NewParents_df.json.gz")
+    compress_json.dump(NewParents_df_j, "subreddit_json_gzs/NewParents_df.json.gz")
 
     InfertilityBabies_df = pd.DataFrame()
     for file in os.listdir("../data/original-reddit/subreddits/InfertilityBabies/submissions/"):
@@ -159,7 +202,7 @@ def main():
 
     InfertilityBabies_df.reset_index(drop=True, inplace=True)
     InfertilityBabies_df_j = InfertilityBabies_df.to_json()
-    compress_json.dump(InfertilityBabies_df_j, "InfertilityBabies_df.json.gz")
+    compress_json.dump(InfertilityBabies_df_j, "subreddit_json_gzs/InfertilityBabies_df.json.gz")
  
     birth_stories_df = pd.DataFrame()
 
@@ -173,11 +216,48 @@ def main():
     birth_stories_df = birth_stories_df.append(NewParents_df, ignore_index=True)
     birth_stories_df = birth_stories_df.append(InfertilityBabies_df, ignore_index=True)
 
+    #label stories as pre or post covid (March 11, 2020)
+    birth_stories_df['date created'] = birth_stories_df['created_utc'].apply(get_post_year)
+    birth_stories_df = birth_stories_df.sort_values(by = 'date created')
+    birth_stories_df['Pre-Covid'] = birth_stories_df['date created'].apply(pandemic)
+
+    missing_text_df = birth_stories_df[birth_stories_df['selftext'].map(lambda x: not x)]
+    missing_id_author_df = missing_text_df[['id', 'author', 'Pre-Covid']]
+    missing_id_author_df['body'] = missing_id_author_df.apply(get_first_comment, axis=1)
+    missing_id_author_df['body'].map(lambda x: x == None).value_counts()
+
+    missing_id_author_df[missing_id_author_df['body'] == None]
+
+    print(birth_stories_df['selftext'].map(lambda x: not x).value_counts())
+    for idx, row in missing_id_author_df.iterrows():
+        birth_stories_df.at[idx, 'selftext'] = row.body
+
+    birth_stories_df['selftext'].map(lambda x: not x).value_counts()
+
+
+    birth_stories_df['selftext'].map(lambda x: x != None).value_counts()
+
+
+    birth_stories_df[birth_stories_df['selftext'].map(lambda x: not not x)]['selftext'].shape
+
+
+    birth_stories_df = birth_stories_df[birth_stories_df['selftext'].map(lambda x: not not x)]
+    birth_stories_df.shape
+
+
+    birth_stories_df['selftext'].map(lambda x: x != '[removed]' or x != '[deleted]').value_counts()
+
+
+    birth_stories_df = birth_stories_df[birth_stories_df['selftext'] != '[removed]']
+    birth_stories_df = birth_stories_df[birth_stories_df['selftext'] != '[deleted]']
+    print(birth_stories_df.shape)
+
     #gets rid of posts that have no content
     nan_value = float("NaN")
     birth_stories_df.replace("", nan_value, inplace=True)
     birth_stories_df.dropna(subset=['selftext'], inplace=True)
 
+    #get story lengths
     birth_stories_df['story length'] = birth_stories_df['selftext'].apply(story_lengths)
 
     #only rows where the story is 500+ words long
@@ -190,6 +270,7 @@ def main():
     warning = 'disclaimer: this is the list that was previously posted'
     birth_stories_df['Valid'] = [findkeyword(sub, warning) for sub in birth_stories_df['selftext']]
     birth_stories_df = birth_stories_df.get(birth_stories_df['Valid'] == False)
+    print(birth_stories_df.shape)
 
     #Convert to compressed json 
     birth_stories_df = birth_stories_df.to_json()
