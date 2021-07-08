7cce1d3a042abfdf0600183d07278c0aae842911
Noreen Mayat <nm3224@barnard.edu>
2021-07-08 11:09:25 -0400

    Opened the jsons

--- src/imports.py
+++ src/imports.py
@@ -21,5 +21,19 @@ import itertools
 import compress_json
 warnings.filterwarnings("ignore")
 
+#Read all relevant dataframe jsons 
+
 birth_stories_df = compress_json.load('birth_stories_df.json.gz')
 birth_stories_df = pd.read_json(birth_stories_df)
+
+labels_df = compress_json.load("labeled_df.json.gz")
+labels_df = pd.read_json(labels_df)
+
+#covid_df = compress_json.load("covid_df.json.gz")
+#covid_df = pd.read_json(covid_df)
+
+#pre_covid_posts_df = compress_json.load("pre_covid_posts_df.json.gz")
+#pre_covid_posts_df = pd.read_json(pre_covid_posts_df)
+
+#post_covid_posts_df = compress_json.load("post_covid_posts_df.json.gz")
+#post_covid_posts_df = pd.read_json(post_covid_posts_df)
