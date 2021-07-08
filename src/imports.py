a5851d88137f4e66727c5b016d93f2c60cf21682
Noreen Mayat <nm3224@barnard.edu>
2021-07-08 15:34:27 -0400

    read note for labeling stories

--- src/imports.py
+++ src/imports.py
@@ -6,19 +6,14 @@ from nltk import ngrams
 from nltk import tokenize
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
 import numpy as np
 from datetime import datetime
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from matplotlib import pyplot as plt
-import matplotlib.dates as mdates
 import itertools
 from itertools import chain, zip_longest
 from little_mallet_wrapper import process_string
-import seaborn as sns
-sns.set(rc={'figure.figsize':(11, 4)})
-from collections import defaultdict
-from tqdm import tqdm
+import seaborn
 import redditcleaner
 import re
 import warnings
@@ -28,14 +23,14 @@ warnings.filterwarnings("ignore")
 
 #Read all relevant dataframe jsons 
 
-#birth_stories_df = compress_json.load('birth_stories_df.json.gz')
-#birth_stories_df = pd.read_json(birth_stories_df)
+birth_stories_df = compress_json.load('birth_stories_df.json.gz')
+birth_stories_df = pd.read_json(birth_stories_df)
 
-#labels_df = compress_json.load("labeled_df.json.gz")
-#labels_df = pd.read_json(labels_df)
+labels_df = compress_json.load("labeled_df.json.gz")
+labels_df = pd.read_json(labels_df)
 
-#pre_covid_posts_df = compress_json.load("pre_covid_posts_df.json.gz")
-#pre_covid_posts_df = pd.read_json(pre_covid_posts_df)
+pre_covid_posts_df = compress_json.load("pre_covid_posts_df.json.gz")
+pre_covid_posts_df = pd.read_json(pre_covid_posts_df)
 
-#post_covid_posts_df = compress_json.load("post_covid_posts_df.json.gz")
-#post_covid_posts_df = pd.read_json(post_covid_posts_df)
+post_covid_posts_df = compress_json.load("post_covid_posts_df.json.gz")
+post_covid_posts_df = pd.read_json(post_covid_posts_df)
