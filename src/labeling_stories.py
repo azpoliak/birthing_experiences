220a057fdaa09ab8bb6104a0ef165fcdd3deb936
Noreen Mayat <nm3224@barnard.edu>
2021-07-08 11:09:03 -0400

    Read things into jsons

--- src/labeling_stories.py
+++ src/labeling_stories.py
@@ -35,38 +35,56 @@ def create_df_label_list(df, column, dct, disallows):
             label_counts.append(df[label].value_counts()[1]) 
     return label_counts
 
-#Things defined outside the main because we need to reference it in other files
+#translate created_utc column into years
+def get_post_year(series):
+    parsed_date = im.datetime.utcfromtimestamp(series)
+    date = parsed_date
+    return date
 
-#Dataframe with only the columns we're working with
-labels_df = im.birth_stories_df[['title', 'selftext']]
-
-#creating lists of words used to assign labels to story titles 
-positive = ['positive']
-not_positive = ['less-than positive']
-negative = ['trauma', 'trigger', 'negative']
-unmedicated = ['no epi', 'natural', 'unmedicated', 'epidural free', 'no meds', 'no pain meds']
-not_unmedicated = ['unnatural']
-medicated = ['epidural', 'epi']
-not_medicated = ['no epi', 'epidural free']
-home = ['home']
-hospital = ['hospital']
-first = ['ftm', 'first time', 'first pregnancy']
-second = ['stm', 'second']
-c_section = ['cesarian', 'section', 'caesar']
-vaginal = ['vaginal', 'vbac']
-
-#applying functions and making a dictionary of the results
-labels_and_n_grams = {'Positive': [positive, not_positive], 'Negative': [negative], 'Unmedicated': [unmedicated, not_unmedicated], 'Medicated': [medicated, not_medicated], 'Home': [home], 'Hospital': [hospital], 'First': [first], 'Second': [second], 'C-Section': [c_section], 'Vaginal': [vaginal]}
-disallows = ['Positive', 'Unmedicated', 'Medicated']
-Covid = {'Covid': ["2019-ncov", "2019ncov", "corona", "coronavirus", "covid", "covid-19", "covid19" "mers", "outbreak", "pandemic", "rona", "sars", "sars-cov-2", "sars2", "sarscov19", "virus", "wuflu", "wuhan"]}
-
-global counts 
-counts = create_df_label_list(labels_df, 'title', labels_and_n_grams, disallows)
-global covid 
-covid = create_df_label_list(labels_df, 'selftext', Covid, [])
-covid_df = labels_df[['title', 'selftext', 'Covid']]
+#True/False column based on before and after pandemic 
+def pandemic(date):
+    start_date = im.datetime.strptime("11 March, 2020", "%d %B, %Y")
+    if date > start_date:
+        return False
+    else:
+        return True
 
 def main():
+    #Dataframe with only the columns we're working with
+    labels_df = im.birth_stories_df[['title', 'selftext']]
+
+    #creating lists of words used to assign labels to story titles 
+    positive = ['positive']
+    not_positive = ['less-than positive']
+    negative = ['trauma', 'trigger', 'negative']
+    unmedicated = ['no epi', 'natural', 'unmedicated', 'epidural free', 'no meds', 'no pain meds']
+    not_unmedicated = ['unnatural']
+    medicated = ['epidural', 'epi']
+    not_medicated = ['no epi', 'epidural free']
+    home = ['home']
+    hospital = ['hospital']
+    first = ['ftm', 'first time', 'first pregnancy']
+    second = ['stm', 'second']
+    c_section = ['cesarian', 'section', 'caesar']
+    vaginal = ['vaginal', 'vbac']
+
+    #applying functions and making a dictionary of the results
+    labels_and_n_grams = {'Positive': [positive, not_positive], 'Negative': [negative], 'Unmedicated': [unmedicated, not_unmedicated], 'Medicated': [medicated, not_medicated], 'Home': [home], 'Hospital': [hospital], 'First': [first], 'Second': [second], 'C-Section': [c_section], 'Vaginal': [vaginal]}
+    disallows = ['Positive', 'Unmedicated', 'Medicated']
+    Covid = {'Covid': ["2019-ncov", "2019ncov", "corona", "coronavirus", "covid", "covid-19", "covid19" "mers", "outbreak", "pandemic", "rona", "sars", "sars-cov-2", "sars2", "sarscov19", "virus", "wuflu", "wuhan"]}
+
+    counts = create_df_label_list(labels_df, 'title', labels_and_n_grams, disallows)
+
+    covid = create_df_label_list(labels_df, 'selftext', Covid, [])
+    covid_df = labels_df[['title', 'selftext', 'Covid']]
+
+    #Read dataframes to compressed json so we can reference them later
+    labels_df = labels_df.to_json()
+    im.compress_json.dump(labels_df, "labeled_df.json.gz")
+
+    covid_df = covid_df.to_json()
+    im.compress_json.dump(covid_df, "covid_df.json.gz")
+
     labels_dict = { 'Labels': list(labels_and_n_grams),
     'Description': ['Positively framed', 'Negatively framed', 'Birth without epidural', 'Birth with epidural',
                  'Birth takes place at home', 'Birth takes place at hospital', 'First birth for the author',
@@ -78,11 +96,30 @@ def main():
     #turn dictionary into a dataframe
     label_counts_df = im.pd.DataFrame(labels_dict, index=im.np.arange(10))
 
-    print(labels_df)
-    print(covid_df.get(covid_df['Covid']==True))
-
     label_counts_df.set_index('Labels', inplace = True)
     #print(label_counts_df)
+
+    im.birth_stories_df['date created'] = im.birth_stories_df['created_utc'].apply(get_post_year)
+    im.birth_stories_df = im.birth_stories_df.sort_values(by = 'date created')
+    im.birth_stories_df['Pre-Covid'] = im.birth_stories_df['date created'].apply(pandemic)
+
+    #Subreddits before pandemic 
+    pre_covid_posts_df = im.birth_stories_df.get(im.birth_stories_df['Pre-Covid']==True)
+    #print(pre_covid_posts_df)
+    #print(f"Subreddits before pandemic: {len(pre_covid_posts_df)}")
+
+    #Convert to Json
+    #pre_covid_posts_df = pre_covid_posts_df.to_json()
+    #im.compress_json.dump(pre_covid_posts_df, "pre_covid_posts_df.json.gz")
+
+    #Subreddits after pandemic 
+    post_covid_posts_df = im.birth_stories_df.get(im.birth_stories_df['Pre-Covid']==False)
+    #print(post_covid_posts_df)
+    #print(f"Subreddits during/after pandemic: {len(post_covid_posts_df)}")
+
+    #Convert to Json
+    #post_covid_posts_df = post_covid_posts_df.to_json()
+    #im.compress_json.dump(post_covid_posts_df, "post_covid_posts_df.json.gz")
 
 if __name__ == "__main__":
     main()
