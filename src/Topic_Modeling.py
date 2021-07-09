f2e909b0bd32fd5aa4701ca6e0167b62496cb4cc
Daphna Spira <des2188@barnard.edu>
2021-07-09 15:15:46 -0400

    adapted to train two dataframes at once

--- src/Topic_Modeling.py
+++ src/Topic_Modeling.py
@@ -29,10 +29,12 @@ def split_story_100_words(story):
         sentiment_story.append(' '.join(s[i:i + n]))
     return sentiment_story
 
-training_chunks = []
-def get_all_chunks(series):
-    for chunk in series:
-        training_chunks.append(chunk)
+def get_all_chunks_from_column(series):
+    #makes list of all chunks from all stories in the df
+    training_chunks = []
+    for story in series:
+        for chunk in story:
+            training_chunks.append(chunk)
     return training_chunks
 
 #splits story into ten equal chunks
@@ -55,10 +57,11 @@ def split_story_10(str):
     return split_story
 
 #makes list of all the chunks for topic inferring
-testing_chunks = []
 def get_chunks(series):
-    for chunk in series:
-        testing_chunks.append(chunk)
+    testing_chunks = []
+    for story in series:
+        for chunk in story:
+            testing_chunks.append(chunk)
     return testing_chunks
 
 #finds average probability for each topic for each chunk of story
@@ -78,84 +81,102 @@ def top_5_keys(lst):
         top5_per_list.append(joined)
     return top5_per_list
 
-def make_plots(df):
+def make_plots(df, df2):
     fig = im.plt.figure()
     ax = fig.add_subplot(111)
     for i in range(topics_over_time_df.shape[1]):
         #ax.clear()
         ax.show()
-        #ax.plot(df.iloc[:, i])
-        #ax.set_title(df.iloc[:, i].name)
-        #ax.set_xlabel('Story Time')
-        #ax.set_ylabel('Topic Probability')
-        #fig.savefig('Topic'+str(i)+'_'+str(df.iloc[:, i].name)+'_Plot.png')
+        ax = ax.plot(df.iloc[:, i])
+        df2.plot(ax=ax)
+        ax.set_title(df.iloc[:, i].name)
+        ax.set_xlabel('Story Time')
+        ax.set_ylabel('Topic Probability')
+        fig.savefig('Topic'+str(i)+'_'+str(df.iloc[:, i].name)+'_Plot.png')
 
 global birth_stories_df_cleaned 
 
 def main():
-    print("Story Stats: ")
-    im.lmw.print_dataset_stats(im.birth_stories_df['selftext'])
+    im.pre_covid_posts_df.name = 'pre_covid'
+    im.post_covid_posts_df.name = 'post_covid'
+    dfs = (im.pre_covid_posts_df, im.post_covid_posts_df)
+    for df in dfs:
+        print("Story Stats: ")
+        im.lmw.print_dataset_stats(df['selftext'])
 
-    #remove emojis, apply redditcleaner, removed stop words
-    im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['selftext'].apply(im.redditcleaner.clean).apply(remove_emojis).apply(process_s)
+        df_name = df.name
 
-    #replace urls with ''
-    im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['Cleaned Submission'].replace(to_replace=r'^https?:\/\/.*[\r\n]*',value='',regex=True)
+        #remove emojis, apply redditcleaner, removed stop words
+        df['Cleaned Submission'] = df['selftext'].apply(im.redditcleaner.clean).apply(remove_emojis).apply(process_s)
 
-    #remove numbers
-    im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['Cleaned Submission'].replace(to_replace=r'NUM*',value='',regex=True)
+        #replace urls with ''
+        df['Cleaned Submission'] = df['Cleaned Submission'].replace(to_replace=r'^https?:\/\/.*[\r\n]*',value='',regex=True)
 
-    birth_stories_df_cleaned = im.birth_stories_df.dropna()
-    
-    #remove any missing values
-    birth_stories_df_cleaned['100 word chunks'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_100_words)
+        #remove numbers
+        df['Cleaned Submission'] = df['Cleaned Submission'].replace(to_replace=r'NUM*',value='',regex=True)
 
-    birth_stories_df_cleaned['100 word chunks'] = birth_stories_df_cleaned['100 word chunks'].apply(get_all_chunks)
+        birth_stories_df_cleaned = df.dropna()
+        
+        #remove any missing values
+        birth_stories_df_cleaned['100 word chunks'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_100_words)
 
-    path_to_mallet = 'mallet-2.0.8/bin/mallet'
+        training_chunks = get_all_chunks_from_column(birth_stories_df_cleaned['100 word chunks'])
 
-    #topic_words, topic_doc_distributions = im.lmw.quick_train_topic_model(path_to_mallet, "topic_modeling", 50, training_chunks)
-    num_topics = 50
+        path_to_mallet = 'mallet-2.0.8/bin/mallet'
 
-    topics = im.lmw.load_topic_keys('topic_modeling/mallet.topic_keys.50')
+        path_to_save = f"topic_modeling_{df_name}"
 
-    #for num_topics, topic in enumerate(topics):
-        #print(f"✨Topic {num_topics}✨ \n\n{topic}\n")
+        #topic_words, topic_doc_distributions = im.lmw.quick_train_topic_model(path_to_mallet, path_to_save, 50, training_chunks)
+        num_topics = 50
 
-    birth_stories_df_cleaned['10 chunks/story'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_10)
+        topics = im.lmw.load_topic_keys(path_to_save+'/mallet.topic_keys.50')
 
-    birth_stories_df_cleaned['10 chunks/story'].apply(get_chunks)
+        #for num_topics, topic in enumerate(topics):
+            #print(f"✨Topic {num_topics}✨ \n\n{topic}\n")
 
-    #infers topics for the documents split into 10 equal chunks based on the topics trained on the 100 word chunks
-    #im.lmw.import_data(path_to_mallet, "topic_modeling_ten_chunks/training_data", "topic_modeling_ten_chunks/formatted_training_data", testing_chunks, training_ids=None, use_pipe_from=None)
-    #im.lmw.infer_topics(path_to_mallet, "topic_modeling/mallet.model.50", "topic_modeling_ten_chunks/formatted_training_data", "topic_modeling_ten_chunks/topic_distributions")
+        birth_stories_df_cleaned['10 chunks/story'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_10)
 
-    #makes df of the probabilities for each topic for each chunk of each story
-    topic_distributions = im.lmw.load_topic_distributions('topic_modeling_ten_chunks/topic_distributions')
-    story_topics_df = im.pd.DataFrame(topic_distributions)
+        testing_chunks = get_chunks(birth_stories_df_cleaned['10 chunks/story'])
 
-    #goes through stories and names them based on the story number and chunk number
-    chunk_titles = []
-    for i in range(len(birth_stories_df_cleaned)-3):
-        for j in range(10):
-            chunk_titles.append(str(i) + ":" + str(j))
+        ten_chunks = f"topic_model_ten_chunks_{df_name}"
 
-    story_topics_df['chunk_titles'] = chunk_titles
+        #infers topics for the documents split into 10 equal chunks based on the topics trained on the 100 word chunks
+        #im.lmw.import_data(path_to_mallet, ten_chunks+"/training_data", ten_chunks+"/formatted_training_data", testing_chunks, training_ids=None, use_pipe_from=None)
+        #im.lmw.infer_topics(path_to_mallet, path_to_save+"/mallet.model.50", ten_chunks+"/formatted_training_data", ten_chunks+"/topic_distributions")
 
-    #groups every ten stories together
-    story_topics_df = story_topics_df.groupby(story_topics_df.index // 10)
+        #makes df of the probabilities for each topic for each chunk of each story
+        topic_distributions = im.lmw.load_topic_distributions(ten_chunks+'/topic_distributions')
+        story_topics_df = f"stories_{df_name}"
+        story_topics_df = im.pd.DataFrame(topic_distributions)
 
-    topics_over_time_df = average_per_chunk(story_topics_df)
+        #goes through stories and names them based on the story number and chunk number
+        chunk_titles = []
+        for i in range(len(birth_stories_df_cleaned)):
+            for j in range(10):
+                chunk_titles.append(str(i) + ":" + str(j))
 
-    #loads topic keys
-    topic_keys = im.lmw.load_topic_keys('topic_modeling/mallet.topic_keys.50')
+        story_topics_df['chunk_titles'] = chunk_titles
 
-    keys_topics = top_5_keys(topic_keys)
+        #groups every ten stories together
+        story_topics_df = story_topics_df.groupby(story_topics_df.index // 10)
 
-    #adds the keys as the names of the topic columns
-    topics_over_time_df.set_axis(keys_topics, axis=1, inplace=True)
+        #topics_over_time_df = None
+        topics_over_time_df = average_per_chunk(story_topics_df)
 
-    print(make_plots(topics_over_time_df))
+        #loads topic keys
+        topic_keys = im.lmw.load_topic_keys(path_to_save+'/mallet.topic_keys.50')
+
+        keys_topics = top_5_keys(topic_keys)
+
+        name = f"{df.name}_topics"
+
+        #adds the keys as the names of the topic columns
+        name = topics_over_time_df.set_axis(keys_topics, axis=1, inplace=True)
+        print(name)
+        
+    #print(pre_covid_posts_df_topics)
+    #print(post_covid_posts_df_topics)
+    #print(make_plots(pre_covid_posts_df_topics, post_covid_posts_df_topics))
 
 if __name__ == "__main__":
     main()
