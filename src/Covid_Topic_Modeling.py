02034434373ca8af6a1bb37b6485588b74dc735b
Noreen Mayat <nm3224@barnard.edu>
2021-07-12 13:40:10 -0400

    Cleaned Topic Model Plots

--- src/Covid_Topic_Modeling.py
+++ src/Covid_Topic_Modeling.py
@@ -96,100 +96,97 @@ def make_plots(df):
     fig = im.plt.figure(figsize=(15,8))
     ax = fig.add_subplot(111)
     for i in range(df.shape[1]):
-        topic_number = i
-        topic_label = df.iloc[:, i].name
-        grouped = df.groupby(df.index)[[topic_label]].mean()
         ax.clear()
-        ax.plot(grouped.iloc[:, i])
-        ax.legend([topic_label])
-        #ax.plot_topics_over_time(topic_distributions, topic_keys, times, topic_index, output_path=None)
+        ax.plot(df.iloc[:, i])
+        ax.legend([df.iloc[:, i].name])
         ax.set_title('Birth Story Topics Over Time')
         ax.set_xlabel('Month')
         ax.set_ylabel('Topic Probability')
-        fig.savefig('Topic_'+str(topic_label)+'_Over_Time.png')
+        fig.savefig('Topic_'+str(df.iloc[:, i].name)+'_Over_Time.png')
 
-global birth_stories_df_cleaned 
+#global birth_stories_df_cleaned 
 
 def main():
-    print("Story Stats: ")
-    im.lmw.print_dataset_stats(im.birth_stories_df['selftext'])
+    #print("Story Stats: ")
+    #im.lmw.print_dataset_stats(im.birth_stories_df['selftext'])
 
     #remove emojis, apply redditcleaner, removed stop words
-    im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['selftext'].apply(im.redditcleaner.clean).apply(remove_emojis).apply(process_s)
+    #im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['selftext'].apply(im.redditcleaner.clean).apply(remove_emojis).apply(process_s)
 
     #replace urls with ''
-    im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['Cleaned Submission'].replace(to_replace=r'^https?:\/\/.*[\r\n]*',value='',regex=True)
+    #im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['Cleaned Submission'].replace(to_replace=r'^https?:\/\/.*[\r\n]*',value='',regex=True)
 
     #remove numbers
-    im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['Cleaned Submission'].replace(to_replace=r'NUM*',value='',regex=True)
+    #im.birth_stories_df['Cleaned Submission'] = im.birth_stories_df['Cleaned Submission'].replace(to_replace=r'NUM*',value='',regex=True)
 
     #remove any missing values
-    birth_stories_df_cleaned = im.birth_stories_df.dropna()
+    #birth_stories_df_cleaned = im.birth_stories_df.dropna()
     
     #split data for training
-    birth_stories_df_cleaned['100 word chunks'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_100_words)
+    #birth_stories_df_cleaned['100 word chunks'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_100_words)
 
     #makes list of all chunks to input into LMW
-    training_chunks = get_all_chunks_from_column(birth_stories_df_cleaned['100 word chunks'])
+    #training_chunks = get_all_chunks_from_column(birth_stories_df_cleaned['100 word chunks'])
 
-    path_to_mallet = 'mallet-2.0.8/bin/mallet'
-    path_to_save = "topic_modeling"
-    num_topics = 50
+    #path_to_mallet = 'mallet-2.0.8/bin/mallet'
+    #path_to_save = "topic_modeling"
+    #num_topics = 50
 
     #train model
     #topic_words, topic_doc_distributions = im.lmw.quick_train_topic_model(path_to_mallet, path_to_save, num_topics, training_chunks)
 
-    birth_stories_df_cleaned['10 chunks/story'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_10)
+    #birth_stories_df_cleaned['10 chunks/story'] = birth_stories_df_cleaned['Cleaned Submission'].apply(split_story_10)
 
-    testing_chunks = get_chunks(birth_stories_df_cleaned['10 chunks/story'])
+    #testing_chunks = get_chunks(birth_stories_df_cleaned['10 chunks/story'])
 
-    ten_chunks = f"topic_modeling_ten_chunks"
+    #ten_chunks = f"topic_modeling_ten_chunks"
 
     #infers topics for the documents split into 10 equal chunks based on the topics trained on the 100 word chunks
     #im.lmw.import_data(path_to_mallet, ten_chunks+"/training_data", ten_chunks+"/formatted_training_data", testing_chunks, training_ids=None, use_pipe_from=None)
     #im.lmw.infer_topics(path_to_mallet, path_to_save+"/mallet.model.50", ten_chunks+"/formatted_training_data", ten_chunks+"/topic_distributions")
 
     #makes df of the probabilities for each topic for each chunk of each story
-    topic_distributions = im.lmw.load_topic_distributions(ten_chunks+'/topic_distributions')
+    #topic_distributions = im.lmw.load_topic_distributions(ten_chunks+'/topic_distributions')
     
-    birth_stories_df_cleaned['topic_distributions'] =  im.pd.Series(topic_distributions)
+    #birth_stories_df_cleaned['topic_distributions'] =  im.pd.Series(topic_distributions)
 
-    story_topics_df = birth_stories_df_cleaned['topic_distributions'].apply(im.pd.Series)
-    story_topics_df = im.pd.DataFrame(topic_distributions)
+    #story_topics_df = birth_stories_df_cleaned['topic_distributions'].apply(im.pd.Series)
+    #story_topics_df = im.pd.DataFrame(topic_distributions)
 
     #goes through stories and names them based on the story number and chunk number
-    chunk_titles = []
-    for i in range(len(birth_stories_df_cleaned)-3):
-        for j in range(10):
-            chunk_titles.append(str(i) + ":" + str(j))
+    #chunk_titles = []
+    #for i in range(len(birth_stories_df_cleaned)-3):
+    #    for j in range(10):
+    #        chunk_titles.append(str(i) + ":" + str(j))
 
-    story_topics_df['chunk_titles'] = chunk_titles
+    #story_topics_df['chunk_titles'] = chunk_titles
 
     #groups every ten stories together
-    story_topics_df.groupby(story_topics_df.index // 10)
+    #story_topics_df.groupby(story_topics_df.index // 10)
 
-    story_topics_df = average_per_story(story_topics_df)
+    #story_topics_df = average_per_story(story_topics_df)
 
     #loads topic keys
-    topic_keys = im.lmw.load_topic_keys(path_to_save+'/mallet.topic_keys.50')
+    #topic_keys = im.lmw.load_topic_keys(path_to_save+'/mallet.topic_keys.50')
 
-    keys_topics = top_5_keys(topic_keys)
+    #keys_topics = top_5_keys(topic_keys)
 
     #adds the keys as the names of the topic columns
-    story_topics_df.set_axis(keys_topics, axis=1, inplace=True)
+    #story_topics_df.set_axis(keys_topics, axis=1, inplace=True)
 
-    birth_stories_df_cleaned.drop(birth_stories_df_cleaned.tail(3).index, inplace = True)
+    #birth_stories_df_cleaned.drop(birth_stories_df_cleaned.tail(3).index, inplace = True)
 
-    birth_stories_df_cleaned.reset_index(drop=True, inplace=True)
-
-    birth_stories_df_cleaned = im.pd.concat([birth_stories_df_cleaned['created_utc'], story_topics_df], axis = 1)
+    #birth_stories_df_cleaned.reset_index(drop=True, inplace=True)
+    #birth_stories_df_cleaned = im.pd.concat([birth_stories_df_cleaned['created_utc'], story_topics_df], axis = 1)
+    #birth_stories_df_cleaned['Date Created'] = birth_stories_df_cleaned['created_utc'].apply(get_post_date)
     
-    birth_stories_df_cleaned['Date Created'] = birth_stories_df_cleaned['created_utc'].apply(get_post_date)
-    #birth_stories_df_cleaned['Date Created'] = [month.to_timestamp() for month in birth_stories_df_cleaned['year-month']]
-
-    birth_stories_df_cleaned.drop(columns='created_utc', inplace=True)
-    birth_stories_df_cleaned = birth_stories_df_cleaned.set_index('Date Created')
-
+    birth_stories_df_cleaned = im.pd.read_csv("birth_stories_df_cleaned.csv")
+    birth_stories_df_cleaned['date'] = im.pd.to_datetime(birth_stories_df_cleaned['Date Created'])
+    birth_stories_df_cleaned['year-month'] = birth_stories_df_cleaned['date'].dt.to_period('M')
+    birth_stories_df_cleaned['Date (by month)'] = [month.to_timestamp() for month in birth_stories_df_cleaned['year-month']]
+    birth_stories_df_cleaned.drop(columns=['Date Created', 'year-month', 'date'], inplace=True)
+    birth_stories_df_cleaned = birth_stories_df_cleaned.set_index('Date (by month)')
+    birth_stories_df_cleaned = im.pd.DataFrame(birth_stories_df_cleaned.groupby(birth_stories_df_cleaned.index).mean())
     make_plots(birth_stories_df_cleaned)
 
 if __name__ == "__main__":
