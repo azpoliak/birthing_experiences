470ee06db3e3fb7cd0f963566e90172d5e1f4718
Noreen Mayat <nm3224@barnard.edu>
2021-07-16 12:44:04 -0400

    P-VALUESgit add src/Sentiment_T_Tests.py  :)

--- src/Sentiment_T_Tests.py
+++ src/Sentiment_T_Tests.py
@@ -2,13 +2,25 @@ import imports as im
 import Test_Sen as ts
 from scipy import stats
 
+def group_raw_scores(df, l):
+	new_df = df[['title', 'selftext']].get(df[l] == True)
+	new_df['tokenized sentences'] = new_df['selftext'].apply(im.tokenize.sent_tokenize)
+	new_df['sentiment groups'] = new_df['tokenized sentences'].apply(ts.split_story_10_sentiment)
+	new_df['comp sent per group'] = new_df['sentiment groups'].apply(ts.per_group, args = ('compound',)) 
+	compressed = im.pd.DataFrame(list(new_df['comp sent per group'])).to_dict(orient='list')
+	raw_score_dict = {} 
+	for key in compressed:
+		raw_score_dict[key] = list(im.itertools.chain.from_iterable(compressed[key])) 
+	return raw_score_dict
+
 def t_test(df_pre, df_post, labels):
 	for label in labels:
-		label_pre = ts.label_frame(df_pre, label, 'Pre-Covid')
-		label_post = ts.label_frame(df_post, label, 'Post-Covid')
-		print(f"{label} Birth: {stats.ttest_ind(label_pre['Sentiments'], label_post['Sentiments'])}")
-
+		label_pre = group_raw_scores(df_pre, label)
+		label_post = group_raw_scores(df_post, label)
+		for key in list(label_pre.keys()):
+			print(f"{label} Birth, Section {key}: {stats.ttest_ind(label_pre[key], label_post[key])}")
 def main():
+	im.progress_bar()
 	labels = list(im.labels_df.columns)
 	labels.remove('title')
 	labels.remove('created_utc')
