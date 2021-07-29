787db55ca678bc25754c0610c9f809688071f446
Daphna Spira <des2188@barnard.edu>
2021-07-29 17:34:47 -0400

    changed font size to 20--still very small. need to be able to access a lot of files on my local machine to run the code

--- src/Topic_Modeling/topic_predictions.py
+++ src/Topic_Modeling/topic_predictions.py
@@ -37,60 +37,58 @@ post_covid_posts_df = compress_json.load("../post_covid_posts_df.json.gz")
 post_covid_posts_df = pd.read_json(post_covid_posts_df)
 
 
-for n in np.arange(5, 55, 5):
+birth_stories_df_cleaned = pd.read_csv(f"../birth_stories_df_cleaned.csv")
 
-    birth_stories_df_cleaned = pd.read_csv(f"birth_stories_df_cleaned_{n}.csv")
+#birth_stories_df_cleaned.reset_index(inplace=True)
+birth_stories_df_cleaned['date'] = pd.to_datetime(birth_stories_df_cleaned['Date Created'])
+birth_stories_df_cleaned['year-month-day'] = birth_stories_df_cleaned['date'].dt.to_period('M')
+birth_stories_df_cleaned['Date'] = [month.to_timestamp() for month in birth_stories_df_cleaned['year-month-day']]
+birth_stories_df_cleaned.drop(columns=['date', 'year-month-day', 'Date Created'], inplace=True)
+birth_stories_df_cleaned['Date'] = birth_stories_df_cleaned['Date'].dt.to_pydatetime()
+birth_stories_df_cleaned = birth_stories_df_cleaned.set_index('Date')
 
-    birth_stories_df_cleaned['date'] = pd.to_datetime(birth_stories_df_cleaned['Date Created'])
-    birth_stories_df_cleaned['year-month'] = birth_stories_df_cleaned['date'].dt.to_period('M')
-    birth_stories_df_cleaned['Date'] = [month.to_timestamp() for month in birth_stories_df_cleaned['year-month']]
-    birth_stories_df_cleaned.drop(columns=['Date Created', 'date', 'year-month', 'Unnamed: 0'], inplace=True)
-    birth_stories_df_cleaned['Date'] = birth_stories_df_cleaned['Date'].dt.to_pydatetime()
-    birth_stories_df_cleaned = birth_stories_df_cleaned.set_index('Date')
+pre_covid = birth_stories_df_cleaned[(birth_stories_df_cleaned.index <= '2020-02-01')]
 
-    pre_covid = birth_stories_df_cleaned[(birth_stories_df_cleaned.index <= '2020-02-01')]
+pre_covid = pd.DataFrame(pre_covid.groupby(pre_covid.index).mean())
+birth_stories_df_cleaned = pd.DataFrame(birth_stories_df_cleaned.groupby(birth_stories_df_cleaned.index).mean())
 
-    pre_covid = pd.DataFrame(pre_covid.groupby(pre_covid.index).mean())
-    birth_stories_df_cleaned = pd.DataFrame(birth_stories_df_cleaned.groupby(birth_stories_df_cleaned.index).mean())
+birth_stories_df_cleaned.to_csv(f'birth_stories_df_topics.csv')
 
-    birth_stories_df_cleaned.to_csv(f'birth_stories_df_topics_{n}.csv')
+if not os.path.exists(f'../../data/Topic_Forecasts'):
+        os.mkdir(f'../../data/Topic_Forecasts')
 
-    if not os.path.exists(f'../../data/{n}_Topic_Forecasts'):
-            os.mkdir(f'../../data/{n}_Topic_Forecasts')
+if not os.path.exists(f'topic_forecasts'):
+        os.mkdir(f'topic_forecasts')
 
-    if not os.path.exists(f'{n}_topic_forecasts'):
-            os.mkdir(f'{n}_topic_forecasts')
+def predict_topic_trend(df, df2):
+    fig = plt.figure(figsize=(15,10))
+    ax = fig.add_subplot(111)
+    for i in range(df.shape[1]):
+        ax.clear()
+        topic_label = df.iloc[:, i].name
+        topic = pd.DataFrame(df.iloc[:,i])
+        topic.reset_index(inplace=True)
+        topic.columns = ['ds', 'y']
+        topic['ds'] = topic['ds'].dt.to_pydatetime()
 
-    def predict_topic_trend(df, df2):
-        fig = plt.figure(figsize=(15,10))
-        ax = fig.add_subplot(111)
-        for i in range(df.shape[1]):
-            ax.clear()
-            topic_label = df.iloc[:, i].name
-            topic = pd.DataFrame(df.iloc[:,i])
-            topic.reset_index(inplace=True)
-            topic.columns = ['ds', 'y']
-            topic['ds'] = topic['ds'].dt.to_pydatetime()
+        actual = pd.DataFrame(df2.iloc[:,i])
+        actual.reset_index(inplace=True)
+        actual.columns = ['ds', 'y']
+        actual['ds'] = actual['ds'].dt.to_pydatetime()
 
-            actual = pd.DataFrame(df2.iloc[:,i])
-            actual.reset_index(inplace=True)
-            actual.columns = ['ds', 'y']
-            print(type(actual['ds'][0]))
-            actual['ds'] = actual['ds'].dt.to_pydatetime()
+        m = Prophet()
+        m.fit(topic)
 
-            m = Prophet(seasonality_mode='multiplicative')
-            m.fit(topic)
+        future = m.make_future_dataframe(periods=15, freq='M')
 
-            future = m.make_future_dataframe(periods=16, freq='MS')
+        forecast = m.predict(future)
+        forecast.to_csv(f'topic_forecasts/{topic_label}_forecasts.csv')
 
-            forecast = m.predict(future)
-            forecast.to_csv(f'{n}_topic_forecasts/{n}_{topic_label}_forecasts.csv')
+        fig1 = m.plot(forecast, xlabel='Date', ylabel='Topic Probability', ax=ax)
+        ax.plot(df2.iloc[:, i], color='k')
+        ax = fig.gca()
+        ax.set_title(f'{topic_label} Forecast', fontsize=20)
+        plt.axvline(pd.Timestamp('2020-03-01'),color='r')
+        fig1.savefig(f'../../data/Topic_Forecasts/{topic_label}_Prediction_Plot.png')
 
-            fig1 = m.plot(forecast, xlabel='Date', ylabel='Topic Probability', ax=ax)
-            ax.plot(df2.iloc[:, i], color='k')
-            ax = fig.gca()
-            ax.set_title(f'{topic_label} Forecast')
-            plt.axvline(pd.Timestamp('2020-03-01'),color='r')
-            fig1.savefig(f'../../data/{n}_Topic_Forecasts/{n}_{topic_label}_Prediction_Plot.png')
-
-    predict_topic_trend(pre_covid, birth_stories_df_cleaned)
+predict_topic_trend(pre_covid, birth_stories_df_cleaned)
