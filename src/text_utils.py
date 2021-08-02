38991baec3b2e8ae75b7faef0a9ccaa7aa1a370b
Daphna Spira <des2188@barnard.edu>
2021-08-02 15:10:53 -0400

    cleaning up code structure

---
+++ src/text_utils.py
@@ -0,0 +1,56 @@
+import nltk
+from nltk import tokenize
+import numpy as np
+import pandas as pd
+
+#splits story into ten equal chunks
+def split_story_10(string):
+    tokenized = tokenize.word_tokenize(string)
+    rounded = round(len(tokenized)/10)
+    if rounded != 0:
+        ind = np.arange(0, rounded*10, rounded)
+        remainder = len(tokenized) % rounded*10
+    else:
+        ind = np.arange(0, rounded*10)
+        remainder = 0
+    split_story = []
+    for i in ind:
+        if i == ind[-1]:
+            split_story.append(' '.join(tokenized[i:i+remainder]))
+            return split_story
+        split_story.append(' '.join(tokenized[i:i+rounded]))
+    return split_story
+
+def make_plots(pre_df, post_df):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i in range(pre_df.shape[1]):
+        ax.clear()
+        persona_label = pre_df.iloc[:, i].name
+        ax.plot(pre_df.iloc[:,i], label = f"Pre-Covid")
+        ax.plot(post_df.iloc[:,i], label = f"During Covid")
+        ax.set_title(f"{persona_label}", fontsize=24)
+        ax.set_xlabel('Story Time')
+        ax.set_ylabel('Persona Frequency')
+        ax.legend()
+        args = get_args()
+        fig.savefig(f'{args.pre_post_plot_output_folder}{persona_label}_pre_post_frequency.png')
+
+#makes plots of persona mention over narrative time for any number of dfs
+def make_n_plots(pre_df, m_j_df, j_n_df, n_a_df, a_j_df):
+    fig = plt.figure(figsize=(15,10))
+    ax = fig.add_subplot(111)
+    for i in range(pre_df.shape[1]):
+        ax.clear()
+        persona_label = pre_df.iloc[:, i].name
+        ax.plot(pre_df.iloc[:,i], label = f"Pre-Covid: Normalized for Story Length")
+        ax.plot(m_j_df.iloc[:,i], label = f"March-June 2020")
+        ax.plot(j_n_df.iloc[:,i], label = f"June-Nov. 2020")
+        ax.plot(n_a_df.iloc[:,i], label = f"Nov. 2020-April 2021")
+        ax.plot(a_j_df.iloc[:,i], label = f"April-June 2021")
+        ax.set_title(f"{persona_label} Presence: Covid-19", fontsize= 20)
+        ax.set_xlabel('Story Time')
+        ax.set_ylabel('Persona Frequency')
+        ax.legend()
+        args = get_args()
+        fig.savefig(f'{args.throughout_covid_output_folder}{persona_label}_throughout_covid_frequency.png')
