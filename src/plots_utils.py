ce16eb3d333716e9176f67b950de3411937fc5ca
Daphna Spira <des2188@barnard.edu>
2021-08-05 19:18:26 -0400

    fixed the code

--- src/plots_utils.py
+++ src/plots_utils.py
@@ -28,3 +28,11 @@ def make_plots(pre_df, post_df=None, throughout=False, m_j_df=None, j_n_df=None,
             fig.savefig(f'{pre_post_plot_output_folder}{persona_label}_pre_post_frequency.png')
         else:
             fig.savefig(f'{throughout_covid_output_folder}{persona_label}_throughout_covid_frequency.png')
+
+#Generates bar graph of number of posts made each month of the pandemic
+def posts_per_month_bar_graph(post_covid_posts_df, bar_graph_output):
+    posts_per_month = post_covid_posts_df['year-month'].value_counts()
+    fig = plt.figure(figsize=(20,10))
+    posts_per_month.sort_index().plot.bar()
+    fig.suptitle('Posts per Month of Covid')
+    fig.savefig(bar_graph_output)
--- src/posts_per_month_during_covid.py
+++ src/posts_per_month_during_covid.py
@@ -13,113 +13,74 @@ import redditcleaner
 import re
 import warnings
 import compress_json
+import json
 warnings.filterwarnings("ignore")
-from date_utils import pandemic_eras
-from date_utils import get_post_month
-from text_utils import story_lengths
-from text_utils import load_data
+from date_utils import pandemic_eras, get_post_month, convert_datetime
+from text_utils import story_lengths, load_data
+from plots_utils import posts_per_month_bar_graph
 import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
 
     #general dfs with story text
-    parser.add_argument("--birth_stories_df", default="birth_stories_df.json.gz", help="path to df with all birth stories", type=str)
-    parser.add_argument("--pre_covid_posts_df", default="relevant_jsons/pre_covid_posts_df.json.gz", help="path to df with all stories before March 11, 2020", type=str)
-    parser.add_argument("--post_covid_posts_df", default="relevant_jsons/post_covid_posts_df.json.gz", help="path to df with all stories on or after March 11, 2020", type=str)
-    parser.add_argument("--labels_df", default="relevant_jsons/labeled_df.json.gz", help="path to df of the stories labeled based on their titles", type=str)
-    '''
-    #New data to create
-    parser.add_argument("--mar_june_2020_df", default="relevant_jsons/mar_june_2020_df.json.gz", help="path to df of the stories from COVID era 1", type=str)
-    parser.add_argument("--june_nov_2020_df", default="relevant_jsons/june_nov_2020_df.json.gz", help="path to df of the stories from COVID era 2", type=str)
-    parser.add_argument("--nov_2020_apr_2021_df", default="relevant_jsons/nov_2020_apr_2021_df.json.gz", help="path to df of the stories from COVID era 3", type=str)
-    parser.add_argument("--apr_june_2021_df", default="relevant_jsons/apr_june_2021_df.json.gz", help="path to df of the stories from COVID era 4", type=str)
-    parser.add_argument("--bar_graph", default="../data/Corpus_Stats_Plots/Posts_per_Month_Covid_bar.png", help="bar graph of number of posts made each month of the pandemic", type=str)
-    '''
-    parser.add_argument("--mar_june_2020_df", default="relevant_jsons/Testing/mar_june_2020_df.json.gz", help="path to df of the stories from COVID era 1", type=str)
-    parser.add_argument("--june_nov_2020_df", default="relevant_jsons/Testing/june_nov_2020_df.json.gz", help="path to df of the stories from COVID era 2", type=str)
-    parser.add_argument("--nov_2020_apr_2021_df", default="relevant_jsons/Testing/nov_2020_apr_2021_df.json.gz", help="path to df of the stories from COVID era 3", type=str)
-    parser.add_argument("--apr_june_2021_df", default="relevant_jsons/Testing/apr_june_2021_df.json.gz", help="path to df of the stories from COVID era 4", type=str)
-    parser.add_argument("--bar_graph", default="relevant_jsons/Testing/Posts_per_Month_Covid_bar.png", help="bar graph of number of posts made each month of the pandemic", type=str)
+    parser.add_argument("--birth_stories_df", default="/home/daphnaspira/birthing_experiences/src/birth_stories_df.json.gz", help="path to df with all birth stories", type=str)
+    parser.add_argument("--pre_covid_posts_df", default="/home/nm3224/birthing_experiences/src/relevant_jsons/pre_covid_posts_df.json.gz", help="path to df with all stories before March 11, 2020", type=str)
+    parser.add_argument("--post_covid_posts_df", default="/home/nm3224/birthing_experiences/src/relevant_jsons/post_covid_posts_df.json.gz", help="path to df with all stories on or after March 11, 2020", type=str)
+    parser.add_argument("--labels_df", default="/home/nm3224/birthing_experiences/src/relevant_jsons/labeled_df.json.gz", help="path to df of the stories labeled based on their titles", type=str)
 
+    #New data to create
+    parser.add_argument("--mar_june_2020_df", default="../data/covid_era_jsons/mar_june_2020_df.json.gz", help="path to df of the stories from COVID era 1", type=str)
+    parser.add_argument("--june_nov_2020_df", default="../data/covid_era_jsons/june_nov_2020_df.json.gz", help="path to df of the stories from COVID era 2", type=str)
+    parser.add_argument("--nov_2020_apr_2021_df", default="../covid_era_jsons/data/nov_2020_apr_2021_df.json.gz", help="path to df of the stories from COVID era 3", type=str)
+    parser.add_argument("--apr_june_2021_df", default="../data/covid_era_jsons/apr_june_2021_df.json.gz", help="path to df of the stories from COVID era 4", type=str)
+    parser.add_argument("--bar_graph_output", default="../data/Corpus_Stats_Plots/Posts_per_Month_Covid_bar.png", help="bar graph of number of posts made each month of the pandemic", type=str)
     args = parser.parse_args()
     return args
-
-#to find the average story length between pre and post covid
-def avg_story_length(dfs):
-    for df in dfs: 
-        df['story length'] = df['selftext'].apply(story_lengths)
-
-        story_lens = list(df['story length'])
-        avg_story_length = np.round(np.mean(story_lens),2)
-
-        print(f'Average story length {df.name}: {avg_story_length}')
-
-#Turns the date column into a year-month datetime object
-def convert_datetime(post_covid_df):
-
-    post_covid_df['Date Created'] = pd.to_datetime(post_covid_df['Date'])
-    post_covid_df['year-month'] = post_covid_df['Date Created'].dt.to_period('M')
-    post_covid_df['year-month'] = [month.to_timestamp() for month in post_covid_df['year-month']]
-    post_covid_df.drop(columns=['Date', 'Date Created'], inplace=True)
-
-#Generates bar graph of number of posts made each month of the pandemic
-def graph(post_covid_df):
-    args = get_args()
-
-    posts_per_month = post_covid_df['year-month'].value_counts()
-    fig = plt.figure(figsize=(20,10))
-    posts_per_month.sort_index().plot.bar()
-    fig.suptitle('Posts per Month of Covid')
-    fig.savefig(args.bar_graph)
     
-
 #Splits df into four eras of covid
-def four_eras(post_covid_df):
-    args = get_args()
+def four_eras(post_covid_posts_df):
+    post_covid_posts_df['Mar 11-June 1 2020'] = post_covid_posts_df['year-month'].apply(pandemic_eras, args = ('2020-03', '2020-06'))
+    post_covid_posts_df['June 1-Nov 1 2020'] = post_covid_posts_df['year-month'].apply(pandemic_eras, args =('2020-06', '2020-11'))
+    post_covid_posts_df['Nov 1 2020-Apr 1 2021'] = post_covid_posts_df['year-month'].apply(pandemic_eras, args = ('2020-11', '2021-04'))
+    post_covid_posts_df['Apr 1-June 24 2021'] = post_covid_posts_df['year-month'].apply(pandemic_eras, args = ('2021-04', '2021-06'))
 
-    post_covid_df['Mar 11-June 1 2020'] = post_covid_df['year-month'].apply(pandemic_eras, args = ('2020-03-01', '2020-06-01')).get(list(post_covid_df.columns))
-    post_covid_df['June 1-Nov 1 2020'] = post_covid_df['year-month'].apply(pandemic_eras, args =('2020-06-01', '2020-11-01')).get(list(post_covid_df.columns))
-    post_covid_df['Nov 1 2020-Apr 1 2021'] = post_covid_df['year-month'].apply(pandemic_eras, args = ('2020-11-01', '2021-04-01')).get(list(post_covid_df.columns))
-    post_covid_df['Apr 1-June 24 2021'] = post_covid_df['year-month'].apply(pandemic_eras, args = ('2021-04-01', '2021-06-01')).get(list(post_covid_df.columns))
+    mar_june_2020_df = post_covid_posts_df.get(post_covid_posts_df['Mar 11-June 1 2020']==True)
+    june_nov_2020_df = post_covid_posts_df.get(post_covid_posts_df['June 1-Nov 1 2020']==True)
+    nov_2020_apr_2021_df = post_covid_posts_df.get(post_covid_posts_df['Nov 1 2020-Apr 1 2021']==True)
+    apr_june_2021_df = post_covid_posts_df.get(post_covid_posts_df['Apr 1-June 24 2021']==True)
 
-    import pdb; pdb.set_trace()
+    print(len(mar_june_2020_df), len(june_nov_2020_df), len(nov_2020_apr_2021_df), len(apr_june_2021_df))
+    return mar_june_2020_df, june_nov_2020_df, nov_2020_apr_2021_df, apr_june_2021_df
 
-    mar_june_2020_df = post_covid_df.get(post_covid_df['Mar 11-June 1 2020']==True)
-    june_nov_2020_df = post_covid_df.get(post_covid_df['June 1-Nov 1 2020']==True)
-    nov_2020_apr_2021_df = post_covid_df.get(post_covid_df['Nov 1 2020-Apr 1 2021']==True)
-    apr_june_2021_df = post_covid_df.get(post_covid_df['Apr 1-June 24 2021']==True)
-
-    #print(len(mar_june_2020_df), len(june_nov_2020_df), len(nov_2020_apr_2021_df), len(apr_june_2021_df))
-
+def save_jsons(mj, jn, na, aj, mar_june_output, june_nov_output, nov_apr_output, apr_june_output):
     #Loads into Jsons
-    mar_june_2020_df = mar_june_2020_df.to_json()
-    compress_json.dump(mar_june_2020_df, args.mar_june_2020_df)
+    mar_june_2020_df = mj.to_json()
+    compress_json.dump(mar_june_2020_df, mar_june_output)
 
-    june_nov_2020_df = june_nov_2020_df.to_json()
-    compress_json.dump(june_nov_2020_df, args.june_nov_2020_df)
+    june_nov_2020_df = jn.to_json()
+    compress_json.dump(june_nov_2020_df, june_nov_output)
 
-    nov_2020_apr_2021_df = nov_2020_apr_2021_df.to_json()
-    compress_json.dump(nov_2020_apr_2021_df, args.nov_2020_apr_2021_df)
+    nov_2020_apr_2021_df = na.to_json()
+    compress_json.dump(nov_2020_apr_2021_df, nov_apr_output)
 
-    apr_june_2021_df = apr_june_2021_df.to_json()
-    compress_json.dump(apr_june_2021_df, args.apr_june_2021_df)
+    apr_june_2021_df = aj.to_json()
+    compress_json.dump(apr_june_2021_df, apr_june_output)
 
 def main():
     args = get_args()
 
-    birth_stories_df, pre_covid_posts_df, post_covid_posts_df, labels_df = load_data(args.birth_stories_df, args.pre_covid_posts_df, args.post_covid_posts_df, args.labels_df)
+    labels_df, birth_stories_df, pre_covid_posts_df, post_covid_posts_df = load_data(args.birth_stories_df, args.pre_covid_posts_df, args.post_covid_posts_df, args.labels_df)
 
     pre_covid_posts_df.name = 'pre-covid'
     post_covid_posts_df.name = 'post-covid'
 
-    #avg_story_length([pre_covid_posts_df, post_covid_posts_df])
+    post_covid_posts_df = convert_datetime(post_covid_posts_df)
 
-    convert_datetime(post_covid_posts_df)
+    posts_per_month_bar_graph(post_covid_posts_df, args.bar_graph_output)
 
-    #graph(post_covid_posts_df)
-
-    four_eras(post_covid_posts_df)
+    mar_june_2020_df, june_nov_2020_df, nov_2020_apr_2021_df, apr_june_2021_df = four_eras(post_covid_posts_df)
+    save_jsons(mar_june_2020_df, june_nov_2020_df, nov_2020_apr_2021_df, apr_june_2021_df, args.mar_june_2020_df, args.june_nov_2020_df, args.nov_2020_apr_2021_df, args.apr_june_2021_df)
 
 if __name__ == '__main__':
     main()
