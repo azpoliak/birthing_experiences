46da2796a514ce31b587b86c04918a34ecab02ae
Noreen Mayat <nm3224@barnard.edu>
2021-08-05 15:23:44 -0400

    Correct sampling

--- src/Samples/Sampling.py
+++ src/Samples/Sampling.py
@@ -20,48 +20,49 @@ import warnings
 import itertools
 import compress_json
 import argparse
+from text_utils import load_data 
+from date_utils import get_post_month
+from topic_utils import average_per_story, top_5_keys, topic_distributions
+
 warnings.filterwarnings("ignore")
 
 def get_args():
     parser = argparse.ArgumentParser()
     #general dfs with story text
-    parser.add_argument("--birth_stories_df", default="../birth_stories_df.json.gz", help="path to df with all birth stories", type=str)
-    parser.add_argument("--pre_covid_df", default="../relevant_jsons/pre_covid_posts_df.json.gz", help="path to df with all stories before March 11, 2020", type=str)
-    parser.add_argument("--post_covid_df", default="../relevant_jsons/post_covid_posts_df.json.gz", help="path to df with all stories on or after March 11, 2020", type=str)
-    parser.add_argument("--labeled_df", default="../relevant_jsons/labeled_df.json.gz", help="path to df of the stories labeled based on their titles", type=str)
-    parser.add_argument("--topic_sample", default="../data/Samples/contractions_topic_sample.xlsx", help="path to sample of topics", type=str)
+    parser.add_argument("--birth_stories_df", default="birth_stories_df.json.gz", help="path to df with all birth stories", type=str)
+    parser.add_argument("--pre_covid_posts_df", default="relevant_jsons/pre_covid_posts_df.json.gz", help="path to df with all stories before March 11, 2020", type=str)
+    parser.add_argument("--post_covid_posts_df", default="relevant_jsons/post_covid_posts_df.json.gz", help="path to df with all stories on or after March 11, 2020", type=str)
+    parser.add_argument("--labels_df", default="relevant_jsons/labeled_df.json.gz", help="path to df of the stories labeled based on their titles", type=str)
+    #parser.add_argument("--topic_sample", default="../data/Samples/contractions_topic_sample.xlsx", help="path to sample of topics", type=str)
+    parser.add_argument("--topic_key_path", default="/home/daphnaspira/birthing_experiences/src/Topic_Modeling/output/50/mallet.topic_keys.50")
+    parser.add_argument("--topic_dist_path", default="/home/daphnaspira/birthing_experiences/src/Topic_Modeling/output/50/mallet.topic_distributions.50")
     args = parser.parse_args()
     return args
 
-#Finds submissions including topic words 
-def findkey(text, labels):
-    x = False
-    for label in labels:
-        if label in text:
-            x = True
-    return x
+def combine_topics_and_months(birth_stories_df, story_topics_df):
+    #makes it even
+    birth_stories_df.drop(birth_stories_df.head(3).index, inplace=True)
+
+    #combines story dates with topic distributions
+    birth_stories_df.reset_index(drop=True, inplace=True)
+    dates_topics_df = pd.concat([birth_stories_df['created_utc', 'title'], story_topics_df], axis=1)
+
+    #converts the date into datetime object for year and month
+    dates_topics_df['Date Created'] = dates_topics_df['created_utc'].apply(get_post_month)
+    dates_topics_df['date'] = pd.to_datetime(dates_topics_df['Date Created'])
+    dates_topics_df.drop(columns=['Date Created', 'created_utc'], inplace=True)
+
+    dates_topics_df = dates_topics_df.set_index('date')
+
+    import pdb; pdb.set_trace()
+    return dates_topics_df
 
 def main():
-	args = get_args()
+    args = get_args()
+    birth_stories_df, pre_covid_posts_df, post_covid_posts_df, labels_df = load_data(args.birth_stories_df, args.pre_covid_posts_df, args.post_covid_posts_df, args.labels_df)
 
-    labels_df = compress_json.load(args.labeled_df)
-    labels_df = pd.read_json(labels_df)
-
-    birth_stories_df = compress_json.load(args.birth_stories_df)
-    birth_stories_df = pd.read_json(birth_stories_df)
-    
-    pre_covid_posts_df = compress_json.load(args.pre_covid_df)
-    pre_covid_posts_df = pd.read_json(pre_covid_posts_df)
-
-    post_covid_posts_df = compress_json.load(args.post_covid_df)
-    post_covid_posts_df = pd.read_json(post_covid_posts_df)
-
-	#create topics below and uncomment last line of code to create excels with these samples
-	topic = ['contractions', 'minutes', 'apart', 'hospital', 'around']
-	post_covid_topics = post_covid_posts_df.get(['selftext', 'Covid', 'title','Date'])
-	post_covid_topics['topic'] = post_covid_posts_df['selftext'].apply(lambda x: findkey(x, topic))
-	topic_sample = post_covid_topics.get(post_covid_topics['topic'] == True).get(post_covid_topics['Covid'] == True).sample(20)
-	#topic_sample.to_excel(args.topic_sample, index = False)
+    story_topics_df = topic_distributions(args.topic_dist_path, args.topic_key_path)
+    dates_topics_df = combine_topics_and_months(birth_stories_df, story_topics_df)
 
 if __name__ == "__main__":
     main()
