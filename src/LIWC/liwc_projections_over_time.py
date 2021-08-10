b19a31d0f0aa0107b62c7cd33ac3646e9acc2fe6
Daphna Spira <des2188@barnard.edu>
2021-08-10 11:45:38 -0400

    update to liwc projections

--- src/LIWC/liwc_projections_over_time.py
+++ src/LIWC/liwc_projections_over_time.py
@@ -6,12 +6,10 @@ from topic_utils import predict_topic_trend_and_plot_significant_differences
 
 def get_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--liwc_scores", default="/home/daphnaspira/birthing_experiences/data/LIWC2015_results_birth_stories_and_ids.csv")
+	parser.add_argument("--liwc_scores", default="/home/daphnaspira/birthing_experiences/data/LIWC2015_results_birth_stories_and_ids.csv", help="path to df with liwc scores for each story")
 	parser.add_argument("--birth_stories_df", default="/home/daphnaspira/birthing_experiences/src/birth_stories_df.json.gz", help="path to df with all birth stories", type=str)    
-	parser.add_argument("--topic_forecasts_plots_output_recent", default="../data/LIWC_Data/LIWC_Forecasts_2019_2021", help="path to where liwc forecast plots are saved")
 	parser.add_argument("--topic_forecasts_plots_output_all", default="../data/LIWC_Data/LIWC_Forecasts_2011_2021", help="path to where liwc forecast plots are saved")
-	parser.add_argument("--ztest_output_recent", default="../data/LIWC_Data/Z_Test_Stats_LIWC_2019_2021.csv")
-	parser.add_argument("--ztest_output_all", default="../data/LIWC_Data/Z_Test_Stats_LIWC_2011_2021.csv")
+	parser.add_argument("--ztest_output_all", default="../data/LIWC_Data/Z_Test_Stats_LIWC_2011_2021.csv", help="path to where ztest scores are saved")
 	args = parser.parse_args()
 	return args
 
@@ -21,20 +19,12 @@ def main():
 	liwc_df = pd.read_csv(args.liwc_scores)
 	dates_topics_df = combine_topics_and_months(args.birth_stories_df, liwc_df, period='M', drop=False)
 	
-	recent_dates_topics_df = combine_topics_and_months(args.birth_stories_df, liwc_df, period='W', drop=False)	
-	recent_dates_topics_df = posts_2019_on(recent_dates_topics_df)
-
 	if not os.path.exists(args.topic_forecasts_plots_output_all):
 		os.mkdir(args.topic_forecasts_plots_output_all)
 
-	if not os.path.exists(args.topic_forecasts_plots_output_recent):
-		os.mkdir(args.topic_forecasts_plots_output_recent)
+	pre_covid = pre_covid_posts(dates_topics_df)
 
-	pre_covid_all = pre_covid_posts(dates_topics_df)
-	pre_covid_2019_on = pre_covid_posts(recent_dates_topics_df)
-
-	#predict_topic_trend_and_plot_significant_differences(pre_covid_all, dates_topics_df, args.topic_forecasts_plots_output_all, args.ztest_output_all)
-	predict_topic_trend_and_plot_significant_differences(pre_covid_2019_on, recent_dates_topics_df, args.topic_forecasts_plots_output_recent, args.ztest_output_recent, periods=73, frequency="W", timestamp="2020-03-11")
+	predict_topic_trend_and_plot_significant_differences(pre_covid, dates_topics_df, args.topic_forecasts_plots_output, args.ztest_output)
 
 if __name__ == "__main__":
     main()
