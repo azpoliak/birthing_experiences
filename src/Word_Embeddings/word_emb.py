3736d79c746bbb88399b160be9192bb6d02508b9
Daphna Spira <des2188@barnard.edu>
2021-08-11 15:42:23 -0400

    update to word embedding

--- src/Word_Embeddings/word_emb.py
+++ src/Word_Embeddings/word_emb.py
@@ -2,67 +2,51 @@ import nltk
 from nltk import tokenize
 from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
-import numpy as np
 import pandas as pd
-from datetime import datetime
-from matplotlib import pyplot as plt
-import little_mallet_wrapper as lmw
-from little_mallet_wrapper import process_string
+import os
 import redditcleaner
-import re
-import compress_json
 import argparse 
-from text_utils import load_subreddits 
+import gensim
+from gensim.test.utils import get_tmpfile
+from gensim.models import Word2Vec
+from text_utils import load_subreddits, load_data_bf
 
 def get_args():
     parser = argparse.ArgumentParser()
     #general dfs with story text
-
-    parser.add_argument("--BabyBumps", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/BabyBumps_all_df.json.gz", help="path to df with all posts from BabyBumps", type=str)
-    parser.add_argument("--beyond_the_bump", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/beyondthebump_all_df.json.gz", help="path to df with all posts from beyond_the_bump", type=str)
-    parser.add_argument("--BirthStories", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/BirthStories_all_df.json.gz", help="path to df with all posts from BirthStories", type=str)
-    parser.add_argument("--daddit", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/daddit_all_df.json.gz", help="path to df with all posts from daddit", type=str)
-    parser.add_argument("--predaddit", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/predaddit_all_df.json.gz", help="path to df with all posts from predaddit", type=str)
-    parser.add_argument("--pregnant", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/pregnant_all_df.json.gz", help="path to df with all posts from pregnant", type=str)
-    parser.add_argument("--Mommit", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/Mommit_all_df.json.gz", help="ppath to df with all posts from Mommit", type=str)
-    parser.add_argument("--NewParents", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/NewParents_all_df.json.gz", help="path to df with all posts from NewParents", type=str)
-    parser.add_argument("--InfertilityBabies", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/InfertilityBabies_all_df.json.gz", help="path to df with all posts from InfertilityBabies", type=str)
-    parser.add_argument("--embedding_results", default="../data/Word_embedding_results/scores_", help="path to where to save results", type=str)
-
+    parser.add_argument("--path", default="/home/daphnaspira/birthing_experiences/data/subreddits_word_embeddings/", help="path to where the subreddits are stored", type=str)
+    parser.add_argument("--subreddit", default="BabyBumps_all_df.json.gz", help="name of the file with the posts of the subreddit to clean and model", type=str)
+    parser.add_argument("--model", default="../data/word2vec_models", help="path to where to save model", type=str)
+    parser.add_argument("--tmpfile", default="../data/word2vec.model", help="path to temp file for gensim model", type=str)
     args = parser.parse_args()
     return args  
 
-def clean_text(dfs):
+def clean_text(df):
+	corpus = []
+	for story in df['selftext']:
+		text = redditcleaner.clean(str(story))
+		tokenized = nltk.sent_tokenize(text)
+		for string in tokenized:
+			word_tokenized = nltk.word_tokenize(string)
+			corpus.append(word_tokenized)
+	return corpus
 
-	corpuses = []
-	for df in dfs:
-		corpus = []
-		for story in df['selftext']:
-			text = redditcleaner.clean(str(story))
-			tokenized = nltk.sent_tokenize(text)
-			for string in tokenized:
-				word_tokenized = nltk.word_tokenize(string)
-				corpus.append(word_tokenized)
-		corpuses.append(corpus)
-	return corpuses
+def train_model(corpus, df_name, tmpfile, model_path):
+	path = get_tmpfile(tmpfile)
+	model = Word2Vec(sentences=corpus, workers=4)
+	model.save(f"{model_path}/{df_name}_word2vec.model")
 
 def main():
 	args = get_args()
 
-	BabyBumps_df, beyond_the_bump_df, BirthStories_df, daddit_df, predaddit_df, pregnant_df, Mommit_df, NewParents_df, InfertilityBabies_df = load_subreddits(args.BabyBumps, args.beyond_the_bump, args.BirthStories, args.daddit, args.predaddit, args.pregnant, args.Mommit, args.NewParents, args.InfertilityBabies)
+	df = load_data_bf(f"{args.path}{args.subreddit}")
+	df_name = args.subreddit.split("_")[0]
 
-	#Set names 
-	BabyBumps_df.name = 'BabyBumps'
-	beyond_the_bump_df.name = 'beyond_the_bump'
-	BirthStories_df.name = 'BirthStories'
-	daddit_df.name = 'daddit'
-	predaddit_df.name = 'predaddit'
-	pregnant_df.name = 'pregnant'
-	Mommit_df.name = 'Mommit'
-	NewParents_df.name = 'NewParents'
-	InfertilityBabies_df.name = 'InfertilityBabies'
+	if not os.path.exists(args.model):
+		os.mkdir(args.model)
 
-	clean_text([BabyBumps_df, beyond_the_bump_df, BirthStories_df, daddit_df, predaddit_df, pregnant_df, Mommit_df, NewParents_df, InfertilityBabies_df])
+	corpus = clean_text(df)
+	train_model(corpus, df_name, args.tmpfile, args.model)
 
 if __name__ == '__main__':
 	main()
